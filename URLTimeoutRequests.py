f1ff8411d2b52f0de2fee053b689cabffba2c68c
Tom Parker-Shemilt <palfrey@tevp.net>
2020-08-09 10:59:54 +0100

    Throw URLTimeoutError if requests throws something

--- URLTimeoutRequests.py
+++ URLTimeoutRequests.py
@@ -3,12 +3,15 @@ from ._URLTimeoutCommon import *
 
 class URLTimeoutRequests(URLGetter):
     def get(self, url, headers={}, ref=None, data=None, ignore_move=False, proxy=None):
-        r = requests.get(url, headers=headers)
+        try:
+            r = requests.get(url, headers=headers)
+        except Exception as e:
+            raise URLTimeoutError(str(e), url)
 
         if r.status_code == 304:
             raise URLOldDataError
 
         if r.status_code != 200:
-            raise URLTimeoutError(str(r.status_code)+" "+r.reason,url, r.status_code)
+            raise URLTimeoutError(str(r.status_code), r.reason,url, r.status_code)
 
         return URLObject(url, None, r.content, r.headers, data)
